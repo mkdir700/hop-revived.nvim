66f3f782a8d990ae863dc19683a4dd2a8752bafc
Keenan Johns <keenanjj13@protonmail.com>
2023-06-06 11:27:05 -0500

    buf: fixed end_col issue

--- lua/hop-revived/defaults.lua
+++ lua/hop-revived/defaults.lua
@@ -2,7 +2,7 @@ local M = {}
 
 M.keys = "asdghklqwertyuiopzxcvbnmfj"
 M.quit_key = "<Esc>"
-M.perm_method = require("hop.perm").TrieBacktrackFilling
+M.perm_method = require("hop-revived.perm").TrieBacktrackFilling
 M.reverse_distribution = false
 M.teasing = true
 M.jump_on_sole_occurrence = true
@@ -11,7 +11,7 @@ M.create_hl_autocmd = true
 M.current_line_only = false
 M.uppercase_labels = false
 M.multi_windows = false
-M.hint_position = require("hop.hint").HintPosition.BEGIN
+M.hint_position = require("hop-revived.hint").HintPosition.BEGIN
 M.hint_offset = 0
 
 return M
